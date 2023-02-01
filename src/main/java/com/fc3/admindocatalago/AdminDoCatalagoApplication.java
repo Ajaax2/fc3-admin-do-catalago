a85da2a92d8e64fc1513883c7c5c3cee57dcda78
Ajaax <ajaax2@hotmail.com>
2023-02-01 08:31:47 -0300

    creating github workflows

---
+++ src/main/java/com/fc3/admindocatalago/AdminDoCatalagoApplication.java
@@ -0,0 +1,13 @@
+package com.fc3.admindocatalago;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminDoCatalagoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminDoCatalagoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fc3/admindocatalago/AdminDoCatalagoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fc3.admindocatalago;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminDoCatalagoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
