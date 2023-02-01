d4700298b082d48ee6082512f3d2ada4d26124d4
Ajaax <ajaax2@hotmail.com>
2023-02-01 10:20:15 -0300

    Created the base to define the objects and classes

--- application/src/main/java/com/fc3/admin/catalogo/application/UseCase.java
+++ application/src/main/java/com/fc3/admin/catalogo/application/UseCase.java
@@ -1,9 +1,9 @@
 package com.fc3.admin.catalogo.application;
 
-import com.fc3.admin.catalago.domain.Category;
+import com.fc3.admin.catalago.domain.category.Category;
 
 public class UseCase {
     public Category execute() {
-        return new Category();
+        return Category.newCategory("teste","teste", true);
     }
 }
